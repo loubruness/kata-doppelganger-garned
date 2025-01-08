b27d55b601bb360bcce7e637d207dbe4545e02bc
Demonis0 <demonisgames@gmail.com>
2025-01-08 15:34:28 +0100

    discount corrected

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -7,14 +7,14 @@ def test_apply_v1():
     discount_applier = DiscountApplier(Notifier())
     doppelgangers = [User(name="Matthieu JACQUES", email="m@m.c"), User(name="Matthiehthhu JACQUES", email="mzdz@m.c"), User(name="Matthiehtddvdvdvdvdvhhu JACQUES", email="mzdvdvdz@m.c")]
     discount_applier.apply_v1(10, doppelgangers)
-    assert discount_applier.notifier.counter == len(doppelgangers)
+    assert discount_applier.notifier.counter == len(doppelgangers), "Not all users were notified"
 
 def test_apply_v2():
     # TODO: write a test that fails due to the bug in
     discount_applier = DiscountApplier(Notifier())
-    doppelgangers = [User(name="Matthieu JACQUES", email="m@m.c"), User(name="Matthiehthhu JACQUES", email="mzdz@m.c"), User(name="Matthiehtddvdvdvdvdvhhu JACQUES", email="mzdvdvdz@m.c")]
+    doppelgangers = [User(name="Riri", email="m@m.c"), User(name="Lou", email="mzdz@m.c"), User(name="Alpha", email="mzdvdvdz@m.c")]
     discount_applier.apply_v2(10, doppelgangers)
-    assert False not in [user.name in discount_applier.notifier.user_sent for user in doppelgangers]
+    assert [x.name for x in doppelgangers] == discount_applier.notifier.user_sent, "Not all users were notified"
 
 class Notifier:
     def __init__(self):
